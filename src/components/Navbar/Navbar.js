07275992bbf564a5c7f55f48b5c219656b35867a
ally-sorloth <allysorloth@gmail.com>
2021-02-08 00:20:15 +0300

    createted custompage

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,6 +10,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import Button from '@material-ui/core/Button';
+import EcoIcon from "@material-ui/icons/Eco";
 import FormGroup from "@material-ui/core/FormGroup";
 import { appContext } from "../../context/AppContext";
 
@@ -32,6 +33,7 @@ export default function MenuAppBar() {
   const open = Boolean(anchorEl);
   const history = useHistory();
   const { token, setToken } = useContext(appContext);
+  const isAuth = localStorage.getItem("token");
 
 //   const handleChange = (event) => {
 //     setAuth(event.target.checked);
@@ -57,7 +59,7 @@ export default function MenuAppBar() {
     setAnchorEl(null);
   }
 
-  const handleLogOut = () => {
+  const handleLogout = () => {
     localStorage.removeItem("token");
     setToken(null);
     history.push("/");
@@ -66,69 +68,134 @@ export default function MenuAppBar() {
 
 
   return (
-    <div className={classes.root}>
-      <FormGroup></FormGroup>      
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton 
-          edge="start" 
-          className={classes.menuButton} 
-          color="inherit" 
-          aria-label="menu"
-          onClick={handleMainPage}>
-           <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title} alignContent="flex-start">
-          Awesome Blog
-          </Typography>
+      <div className={classes.root}>
+        <FormGroup></FormGroup>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMainPage}
+            >
+              <EcoIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Awesome Blog
+            </Typography>
+            {token ? (
+              <div>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  style={{ marginTop: "3.2rem" }}
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
+                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </Menu>
+              </div>
+            ) : (
+              <>
+                <Button onClick={() => history.push("/login")} color="inherit">
+                  Login
+                </Button>
+                <Button onClick={() => history.push("/register")} color="inherit">
+                  Register
+                </Button>
+              </>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+  
+
+
+//     <div className={classes.root}>
+//       <FormGroup></FormGroup>      
+//       <AppBar position="static">
+//         <Toolbar>
+//           <IconButton 
+//           edge="start" 
+//           className={classes.menuButton} 
+//           color="inherit" 
+//           aria-label="menu"
+//           onClick={handleMainPage}>
+//            <MenuIcon />
+//           </IconButton>
+//           <Typography variant="h6" className={classes.title} alignContent="flex-start">
+//           Awesome Blog
+//           </Typography>
           
-          {token ? (
-            <div>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                {/* {username} */}
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-                <MenuItem onClick={handleLogOut}>Logout</MenuItem>
-              </Menu>
-            </div>
-          ) : (
-            <>
-            <Button onClick={() => history.push("/login")} color="inherit">
-                Login
-              </Button>
-              <Button onClick={() => history.push("/register")} color="inherit">
-                Register
-              </Button>
-            </>
-          )};
+//           {token ? (
+//             <div>
+//               <IconButton
+//                 aria-label="account of current user"
+//                 aria-controls="menu-appbar"
+//                 aria-haspopup="true"
+//                 onClick={handleMenu}
+//                 color="inherit"
+//               >
+//                 {/* {username} */}
+//                 <AccountCircle />
+//               </IconButton>
+//               <Menu
+//                 id="menu-appbar"
+//                 anchorEl={anchorEl}
+//                 anchorOrigin={{
+//                   vertical: 'top',
+//                   horizontal: 'right',
+//                 }}
+//                 keepMounted
+//                 transformOrigin={{
+//                   vertical: 'top',
+//                   horizontal: 'right',
+//                 }}
+//                 open={open}
+//                 onClose={handleClose}
+//               >
+//                 <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
+//                 <MenuItem onClick={handleClose}>My account</MenuItem>
+//                 <MenuItem onClick={handleLogOut}>Logout</MenuItem>
+//               </Menu>
+//             </div>
+//           ) : (
+//             <>
+//             <Button onClick={() => history.push("/login")} color="inherit">
+//                 Login
+//               </Button>
+//               <Button onClick={() => history.push("/register")} color="inherit">
+//                 Register
+//               </Button>
+//             </>
+//           )};
            
-            {/* // <Menu onClick={() => {window.location.href = "/login"}}>Sign in</Menu>
-            // <Menu onClick={() => {window.location.href = "/register"}}>Sign up</Menu>          */}
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-}
+//             {/* // <Menu onClick={() => {window.location.href = "/login"}}>Sign in</Menu>
+//             // <Menu onClick={() => {window.location.href = "/register"}}>Sign up</Menu>          */}
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
