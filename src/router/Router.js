1fd7ae1c19c1568298c77a8a55d014dd31671fa3
ally-sorloth <allysorloth@gmail.com>
2021-02-18 22:01:04 +0300

    corrected Router js and add privaterouter

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,25 +1,33 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from "react-router-dom";
 import MainPage from "../pages/HomePage/MainPage";
 import RegisterPage from "../pages/RegisterPage/RegisterPage";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import Navbar from "../components/Navbar/Navbar";
 import { CustomPostPage } from "../pages/CustomPostPage/CustomPostPage";
 import { PostDetail } from "../pages/PostDetail/PostDetail";
+import PrivateRouter from "./PrivateRouter";
+import { Suspense } from "react";
 
 
 function AppRouter() {
   return (
-    <Router>
-      <Navbar/>
-      <Switch>
-          <Route exact path="/register" component={RegisterPage}/>
-          <Route exact path="/login" component={LoginPage}/>
-          <Route exact path="/detail/:slug" component={PostDetail}/>
-          <Route exact path="/create" component={CustomPostPage} />
-          <Route exact path="/edit/:slug" component={CustomPostPage} />
-          <Route path="/" component={MainPage}/>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Suspense fallback="">
+        <Router>
+          <Navbar/>
+          <Switch>
+              <Route path="/register" component={RegisterPage} exact/>
+              <Route path="/login" component={LoginPage} exact/>
+              <Route path="/" component={MainPage} exact/>
+              <PrivateRouter exact path="/detail/:slug" component={PostDetail}/>
+              <PrivateRouter exact path="/profile" component={ProfilePage}/>
+
+              <Route exact path="/create" component={CustomPostPage} />
+              <Route exact path="/edit/:slug" component={CustomPostPage} />
+          </Switch>
+        </Router>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
