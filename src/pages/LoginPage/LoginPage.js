31f0aae01c2c5e50193efc69ee65f61a800dde4c
ally-sorloth <allysorloth@gmail.com>
2021-03-07 23:17:40 +0300

    corrected loginpage

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -129,145 +129,145 @@ function LoginPage () {
   // };
 
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOpenIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log In
-          </Typography>
-          <form onSubmit={formik.handleSubmit}>
-            <TextField
-              fullWidth
-              margin="normal"
-              id="email"
-              name="email"
-              label="Email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-            />
-            <TextField
-              fullWidth
-              margin="normal"
-              id="password"
-              name="password"
-              label="Password"
-              type="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-            />
-            <Button
-              color="primary"
-              variant="contained"
-              fullWidth
-              type="submit"
-              className={classes.submit}
-            >
-              Login
-            </Button>
-          </form>
-          {/*           <Grid container className={classes.linkWrapper}>
-            <Grid item xs textAlign="center">
-              <Link href="#" variant="body2" mx="auto">
-                Forgot password?
-              </Link>
-            </Grid>
-          </Grid> */}
-          <Grid container className={classes.linkWrapper}>
-            <Grid item xs>
-              <Link href="/register" variant="body2" mx="auto">
-                Don't have an account? Sign Up
-              </Link>
-            </Grid>
-          </Grid>
-          <Box mt={5}>
-            <Typography variant="body2" color="textSecondary" align="center">
-              {"Copyright © "}
-              <Link color="inherit" href="/">
-                Our Website
-              </Link>{" "}
-              {new Date().getFullYear()}
-              {"."}
-            </Typography>
-          </Box>
-        </div>
-      </Grid>
-    </Grid>
-
-    // <Container className={signinStyles.wrapper} maxWidth="sm">
-    //   <Avatar className={signinStyles.avatar}>
-    //     <LockOutlinedIcon />
-    //   </Avatar>
-    //   <Typography className={signinStyles.signIn} variant="h4">
-    //     Sign In
-    //   </Typography>
-    //   <Formik
-    //     initialValues={initialValues}
-    //     validationSchema={signInValidationSchema}
-    //     onSubmit={formik.handleSubmit}
-    //   >
-    //     {({ handleSubmit, handleChange, values, errors }) => (
-    //       <form onSubmit={handleSubmit}>
-    //         <Grid container spacing={3}>
-    //           <Grid item xs={12}>
-    //             <TextField
-    //               name="email"
-    //               label="Email"
-    //               variant="outlined"
-    //               fullWidth
-    //               value={values.email}
-    //               onChange={handleChange}
-    //               error={errors.email}
-    //               helperText={errors.email}
-    //             />
-    //           </Grid>
-    //           <Grid item xs={12}>
-    //             <TextField
-    //               name="password"
-    //               label="Password"
-    //               variant="outlined"
-    //               type="password"
-    //               fullWidth
-    //               value={values.password}
-    //               onChange={handleChange}
-    //               error={errors.password}
-    //               helperText={errors.password}
-    //             />
-    //           </Grid>
-    //           <Grid item xs={12}>
-    //             <Button
-    //               type="submit"
-    //               variant="contained"
-    //               color="primary"
-    //               fullWidth
-    //             >
-    //               Login
-    //             </Button>
-    //           </Grid>
-    //           {/* <Grid item xs={12}>
-    //             <Button
-    //               variant="contained"
-    //               color="primary"
-    //               fullWidth
-    //               onClick={handleGoogleButtonClick}
-    //             >
-    //               Sign In with Google
-    //             </Button>
-    //           </Grid> */}
-    //         </Grid>
-    //         <p style={{textAlign:"center",color:"red"}}><small>{loginError}</small></p>
+    // <Grid container component="main" className={classes.root}>
+    //   <CssBaseline />
+    //   <Grid item xs={false} sm={4} md={7} className={classes.image} />
+    //   <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+    //     <div className={classes.paper}>
+    //       <Avatar className={classes.avatar}>
+    //         <LockOpenIcon />
+    //       </Avatar>
+    //       <Typography component="h1" variant="h5">
+    //         Log In
+    //       </Typography>
+    //       <form onSubmit={formik.handleSubmit}>
+    //         <TextField
+    //           fullWidth
+    //           margin="normal"
+    //           id="email"
+    //           name="email"
+    //           label="Email"
+    //           value={formik.values.email}
+    //           onChange={formik.handleChange}
+    //           error={formik.touched.email && Boolean(formik.errors.email)}
+    //           helperText={formik.touched.email && formik.errors.email}
+    //         />
+    //         <TextField
+    //           fullWidth
+    //           margin="normal"
+    //           id="password"
+    //           name="password"
+    //           label="Password"
+    //           type="password"
+    //           value={formik.values.password}
+    //           onChange={formik.handleChange}
+    //           error={formik.touched.password && Boolean(formik.errors.password)}
+    //           helperText={formik.touched.password && formik.errors.password}
+    //         />
+    //         <Button
+    //           color="primary"
+    //           variant="contained"
+    //           fullWidth
+    //           type="submit"
+    //           className={classes.submit}
+    //         >
+    //           Login
+    //         </Button>
     //       </form>
-    //     )}
-    //   </Formik>
-    // </Container>
+    //       {/*           <Grid container className={classes.linkWrapper}>
+    //         <Grid item xs textAlign="center">
+    //           <Link href="#" variant="body2" mx="auto">
+    //             Forgot password?
+    //           </Link>
+    //         </Grid>
+    //       </Grid> */}
+    //       <Grid container className={classes.linkWrapper}>
+    //         <Grid item xs>
+    //           <Link href="/register" variant="body2" mx="auto">
+    //             Don't have an account? Sign Up
+    //           </Link>
+    //         </Grid>
+    //       </Grid>
+    //       <Box mt={5}>
+    //         <Typography variant="body2" color="textSecondary" align="center">
+    //           {"Copyright © "}
+    //           <Link color="inherit" href="/">
+    //             Our Website
+    //           </Link>{" "}
+    //           {new Date().getFullYear()}
+    //           {"."}
+    //         </Typography>
+    //       </Box>
+    //     </div>
+    //   </Grid>
+    // </Grid>
+
+    <Container className={signinStyles.wrapper} maxWidth="sm">
+      <Avatar className={signinStyles.avatar}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography className={signinStyles.signIn} variant="h4">
+        Sign In
+      </Typography>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={signInValidationSchema}
+        onSubmit={formik.handleSubmit}
+      >
+        {({ handleSubmit, handleChange, values, errors }) => (
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  value={values.email}
+                  onChange={handleChange}
+                  error={errors.email}
+                  helperText={errors.email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password"
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  fullWidth
+                  value={values.password}
+                  onChange={handleChange}
+                  error={errors.password}
+                  helperText={errors.password}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Login
+                </Button>
+              </Grid>
+              {/* <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={handleGoogleButtonClick}
+                >
+                  Sign In with Google
+                </Button>
+              </Grid> */}
+            </Grid>
+            <p style={{textAlign:"center",color:"red"}}><small>{loginError}</small></p>
+          </form>
+        )}
+      </Formik>
+    </Container>
   );
 }
 
